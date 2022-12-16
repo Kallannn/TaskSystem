f8e0ffe9e51c855a43cd65d53537af74a2a04778
Gustavo Henrique Krueger <78285873+Kallannn@users.noreply.github.com>
2022-12-16 12:23:12 -0300

    Adicionar arquivos de projeto.

---
+++ TaskSystem/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskSystem.Models;
+using TaskSystem.Repositories;
+using TaskSystem.Repositories.Interfaces;
+
+namespace TaskSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UserModel>>> GetAllUsers()
+        {
+            List<UserModel> users = await _userRepository.GetAllUsers();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserModel>> GetById(int id)
+        {
+            UserModel user = await _userRepository.GetById(id);
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserModel>> CreateUser([FromBody] UserModel user)
+        {
+            UserModel newUser = await _userRepository.Add(user);
+            return Ok(newUser);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UserModel>> UpdateUser([FromBody] UserModel user, int id)
+        {
+            user.Id = id;
+            UserModel updatedUser = await _userRepository.Update(user, id);
+            return Ok(updatedUser);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UserModel>> DeleteUser(int id)
+        {
+            bool deleted = await _userRepository.Delete(id);
+            return Ok(deleted);
+        }
+    }
+}
---
+++ TaskSystem/Enums/TaskStatus.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace TaskSystem.Enums
+{
+    public enum TaskStatus
+    {
+        [Description("To do")]
+        ToDo = 1,
+
+        [Description("On Going")]
+        OnGoing = 2,
+        [Description("Finished")]
+        Finished = 3
+    }
+}
